006cb9e2a0b8114da6bb878f25e9fb1edb9e38fc
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2024-12-01 23:09:02 -0500

    added schemas

---
+++ connect-react/database_files/models/messageSchema.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+import {IUser} from "./userSchema"
+
+export interface Message extends Document {
+  _id: string;
+  sender: IUser;
+  content: string; // Use the imported uUser interface
+}
+const MessageSchema = new Schema<Message>({
+  _id: {
+    type: String, // Use a string for the custom ID
+    required: true
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Message",
+  },
+  content: {
+    type: String,
+    required: true
+  }
+});
+
+
+
+const Message: Model<Message> =
+  mongoose.models.Message || mongoose.model<Message>("Message", MessageSchema);
+
+export default Message;
